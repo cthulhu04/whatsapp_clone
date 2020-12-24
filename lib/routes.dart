aada6066eed2cbb483aa403f3485fdf726e2f04a
diego bleecke <diego4@live.nl>
2020-12-24 20:42:08 +0100

    project

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,10 +3,6 @@ import 'package:whatsapp_clone/constants/route_constant.dart';
 import 'package:whatsapp_clone/screen/login_screen.dart';
 import 'package:whatsapp_clone/screen/undefined_screen.dart';
 
-// deze router navigatie zorg er voor wanneer de begruiker en knop klik
-// naar welke scherm ze gaan door de naar toe tevoegen voor de
-// scherm....?
-
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case Login:
